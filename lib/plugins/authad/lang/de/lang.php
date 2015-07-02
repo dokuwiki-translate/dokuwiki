d8ee4496d74db1accf6902722d5674ca7f7fd231
rnck <dokuwiki@rnck.de>
2015-07-02 15:46:08 +0200

    translation update

--- lib/plugins/authad/lang/de/lang.php
+++ lib/plugins/authad/lang/de/lang.php
@@ -4,6 +4,9 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Andreas Gohr <gohr@cosmocode.de>
+ * @author rnck <dokuwiki@rnck.de>
  */
 $lang['domain']                = 'Anmelde-Domäne';
 $lang['authpwdexpire']         = 'Ihr Passwort läuft in %d Tag(en) ab. Sie sollten es frühzeitig ändern.';
+$lang['passchangefail']        = 'Das Passwort konnte nicht geändert werden. Eventuell wurde die Passwort-Richtlinie nicht erfüllt.';
+$lang['connectfail']           = 'Eine Verbindung zum Active Directory Server konnte nicht hergestellt werden.';
--- lib/plugins/authmysql/lang/de/lang.php
+++ lib/plugins/authmysql/lang/de/lang.php
@@ -5,7 +5,9 @@
  * 
  * @author Noel Tilliot <noeltilliot@byom.de>
  * @author Hendrik Diel <diel.hendrik@gmail.com>
+ * @author rnck <dokuwiki@rnck.de>
  */
+$lang['connectfail']           = 'Datenbankverbindung konnte nicht hergestellt werden.';
 $lang['userexists']            = 'Entschuldigung, aber dieser Benutzername ist bereits vergeben.';
 $lang['usernotexists']         = 'Sorry, dieser Nutzer existiert nicht.';
 $lang['writefail']             = 'Die Benutzerdaten konnten nicht geändert werden. Bitte wenden Sie sich an den Wiki-Admin.';
