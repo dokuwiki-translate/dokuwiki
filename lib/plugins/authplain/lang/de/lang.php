c753bf3856a617a2aa9c26dbc2e118bca2b803e4
Napf <bei.fragen@sags-per-mail.de>
2016-06-17 18:36:32 +0200

    translation update

--- lib/plugins/authplain/lang/de/lang.php
+++ lib/plugins/authplain/lang/de/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Anika Henke <anika@selfthinker.org>
  */
 $lang['userexists']            = 'Der Benutzername existiert leider schon.';
